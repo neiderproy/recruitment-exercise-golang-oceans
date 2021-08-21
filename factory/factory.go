e123fded34f5e03f10d924952c7c6da6a0544a14
neiderproy <hsneider_salgado14@hotmail.com>
2021-08-21 07:56:08 -0500

    ajustements for factory

--- factory/factory.go
+++ factory/factory.go
@@ -1,9 +1,10 @@
 package factory
 
 import (
+	"errors"
 	"fmt"
 	"github.com/neiderproy/recruitment-exercise-golang-oceans/assemblyspot"
-	"github.com/neiderproy/recruitment-exercise-golang-oceans/vehicle"
+	 vehicleCar "github.com/neiderproy/recruitment-exercise-golang-oceans/vehicle"
 )
 
 const assemblySpots int = 5
@@ -31,10 +32,18 @@ func New() *Factory {
 
 	return factory
 }
+type LogAssembled struct {
+	VehicleID int
+	LogTest string
+	LogAss string
+}
 
 //HINT: this function is currently not returning anything, make it return right away every single vehicle once assembled,
 //(Do not wait for all of them to be assembled to return them all, send each one ready over to main)
-func (f *Factory) StartAssemblingProcess(amountOfVehicles int) {
+func (f *Factory) StartAssemblingProcess(amountOfVehicles int ,log chan <-*LogAssembled, err chan <-error, isDone chan <- bool) {
+	if amountOfVehicles <= 0{
+		err <- errors.New("not valid amount")
+	}
 	vehicleList := f.generateVehicleLots(amountOfVehicles)
 	
 	for _, vehicle := range vehicleList {
@@ -42,26 +51,37 @@ func (f *Factory) StartAssemblingProcess(amountOfVehicles int) {
 
 		idleSpot := <-f.AssemblingSpots
 		idleSpot.SetVehicle(&vehicle)
-		vehicle, err := idleSpot.AssembleVehicle()
 
-		if err != nil {
-			continue
+		car:= make(chan *vehicleCar.Car)
+		errorCh  := make(chan error)
+		go idleSpot.AssembleVehicle(car,errorCh)
+
+		select {
+			case vehicle := <- car:
+				vehicle.TestingLog = f.testCar(vehicle)
+				vehicle.AssembleLog = idleSpot.GetAssembledLogs()
+				idleSpot.SetVehicle(nil)
+
+				log <- &LogAssembled{
+					VehicleID: vehicle.Id,
+					LogTest:   vehicle.TestingLog,
+					LogAss:    vehicle.AssembleLog,
+				}
+			case assembleVehicleErr := <- errorCh:
+				err <- assembleVehicleErr
+
 		}
 
-		vehicle.TestingLog = f.testCar(vehicle)
-		vehicle.AssembleLog = idleSpot.GetAssembledLogs()
-
-		idleSpot.SetVehicle(nil)
-		f.AssemblingSpots <- idleSpot
 	}
+	isDone <- true
 }
 
-func (Factory) generateVehicleLots(amountOfVehicles int) []vehicle.Car {
-	var vehicles = []vehicle.Car{}
+func (Factory) generateVehicleLots(amountOfVehicles int) []vehicleCar.Car {
+	var vehicles = []vehicleCar.Car{}
 	var index = 0
 
 	for {
-		vehicles = append(vehicles, vehicle.Car{
+		vehicles = append(vehicles, vehicleCar.Car{
 			Id:            index,
 			Chassis:       "NotSet",
 			Tires:         "NotSet",
@@ -83,7 +103,7 @@ func (Factory) generateVehicleLots(amountOfVehicles int) []vehicle.Car {
 	return vehicles
 }
 
-func (f *Factory) testCar(car *vehicle.Car) string {
+func (f *Factory) testCar(car *vehicleCar.Car) string {
 	logs := ""
 
 	log, err := car.StartEngine()
